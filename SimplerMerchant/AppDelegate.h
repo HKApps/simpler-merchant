26ffb95e0689dd7076d6a4eb32200d6f04005853
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-03-08 17:01:47 -0500

    Initial Commit

---
+++ SimplerMerchant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimplerMerchant
+//
+//  Created by Basil Siddiqui on 3/8/13.
+//  Copyright (c) 2013 Basil Siddiqui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimplerMerchant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimplerMerchant
+//
+//  Created by Basil Siddiqui on 3/8/13.
+//  Copyright (c) 2013 Basil Siddiqui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
